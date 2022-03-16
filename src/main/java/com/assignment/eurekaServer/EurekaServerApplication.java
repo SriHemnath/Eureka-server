0232f14bfe2210a71d6583073c46d56b1ef1f366
HEMNATH-PC\Hemnath <hemnath.j1995@gmail.com>
2022-03-16 12:22:07 +0530

    first commit

---
+++ src/main/java/com/assignment/eurekaServer/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.assignment.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Service registry and discovery service
+ *
+ * @author Hemnath Jaishankar
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
